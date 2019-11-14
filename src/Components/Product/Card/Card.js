68090faea2e7cb2dac4694ed80f564a060dae55d
Evgeny T <official.trusov.mail@gmail.com>
2019-11-14 20:30:06 +0700

    Added dummy product card component.

---
+++ src/Components/Product/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Card = () => (
+    <div>
+        <p>Название товара</p>
+        <p>Количество на складе</p>
+        <p>Цена (р.)</p>
+    </div>
+)
+
+export default Card;
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import Card from './Product/Card/Card';
+
+export {
+    Card
+}
