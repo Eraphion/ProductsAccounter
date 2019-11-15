e0530d6abf8d3eda8bebe1bb7425782a8131e72f
Evgeny T <official.trusov.mail@gmail.com>
2019-11-16 00:15:10 +0700

    Added filter feature for product table

--- src/Components/Product/CardList/Component.js
+++ src/Components/Product/CardList/Component.js
@@ -3,14 +3,18 @@ import {useSelector} from 'react-redux';
 
 import {Card} from 'Components';
 
-const CardList = () => {
+const CardList = ({isFiltered, filterName}) => {
     const cardsSrc = useSelector(state => state.products)
     if (!cardsSrc)
         return <div>Подождите, данные загружаются...</div>
         
-    const cardsToRender = cardsSrc.map(card => (
-        <Card key={card.id} {...card}/>
-    ))
+    const cardsToRender = cardsSrc.map(card => {
+        if (isFiltered === true && 
+            !card.name.toLowerCase().includes(filterName.toLowerCase()))
+            return null;
+        else 
+            return <Card key={card.id} {...card}/>
+    })
 
     return (
         <ul>
--- src/Components/ProductTable/Component.js
+++ src/Components/ProductTable/Component.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
+import {useState} from 'react';
 
 import {CardList} from 'Components';
 import {getNavigation} from 'Routing';
 
 const ProductTable = () => {
 const {toAddProduct} = getNavigation(useHistory());
+const [isFiltered, setFiltered] = useState(false);
+const [filterName, setFilterName] = useState("");
+
+const filterOptions = {isFiltered, filterName};
+
     return (
         <div>
-            <CardList />
+            <div>
+                <span>Применить фильтр по имени</span>
+                <input 
+                    placeholder="Название товара"
+                    value={filterName}
+                    onChange={(e) => setFilterName(e.target.value)}
+                ></input>
+
+                <input type="checkbox" value={isFiltered} onClick={() => setFiltered(!isFiltered)}></input>
+            </div>
+            <CardList {...filterOptions}/>
             <button onClick={toAddProduct}>Добавить товар</button>
         </div>
     );
