52b7a63bd77359cb2bb175423da7688ba7c174fb
Evgeny T <official.trusov.mail@gmail.com>
2019-11-16 09:22:18 +0700

    Added css library

--- src/Components/ProductTable/Component.js
+++ src/Components/ProductTable/Component.js
@@ -15,14 +15,14 @@ const filterOptions = {isFiltered, filterName};
     return (
         <div>
             <div>
-                <span>Применить фильтр по имени</span>
+                <span>Фильтр по имени</span>
                 <input 
                     placeholder="Название товара"
                     value={filterName}
                     onChange={(e) => setFilterName(e.target.value)}
                 ></input>
 
-                <input type="checkbox" value={isFiltered} onClick={() => setFiltered(!isFiltered)}></input>
+                <label><input type="checkbox" value={isFiltered} onClick={() => setFiltered(!isFiltered)}></input><span>Применить</span></label>
             </div>
             <ProductList {...filterOptions}/>
             <button onClick={toAddProduct}>Добавить товар</button>
